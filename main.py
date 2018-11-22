585eadbb063c27fd2415e508163c2bef2e1e62e7
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-11-22 13:44:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def about():
   await client.say("Fan made D&D discord bot!, started working on 18-11-2018")
 
 @client.command(brief="Roll a certain stat for example: dexterity")
-async def stat(modifier):
+async def stat(modifier=None):
     if modifier is None:
         rolls = []
         for i in range(4):
