a90ab2677bcb4750a4b75749ba216e77da39c132
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-11-26 19:41:47 +0100

    Update main.py
    daan saved the day

--- main.py
+++ main.py
@@ -205,7 +205,7 @@ async def stats():
         rolls.sort()
         variabel6 = sum(rolls[-3:])    
 
-await client.say(variabel1, variabel2, variabel3, variabel4, variabel5, variabel6)
+    await client.say(variabel1, variabel2, variabel3, variabel4, variabel5, variabel6)
         
                                  
 async def list_servers():
