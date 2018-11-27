649dfc8b34d5243132e7ef03b74c46e9e34c82ad
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-11-27 15:07:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -209,29 +209,61 @@ async def stat(modifier=None):
     if modifier is None:
         rolls = []
         for i in range(4):
-            rolls.append(randrange(1,6))
+            rolls.append(randrange(1,7))
         rolls.sort()
         variabel = sum(rolls[-3:])
         await client.say("After rolling 4 times your roll became a: " + str(variabel))
     else:
         rolls = []
         for i in range(4):
-            rolls.append(randrange(1,6))
+            rolls.append(randrange(1,7))
         rolls.sort()
         variabel = sum(rolls[-3:]) + int(modifier)
         await client.say("After rolling 4 times your roll became a: " + str(variabel))
       
 @client.command(brief="Roll all your stats")
 async def stats():
-	rolls = []
-	variables = []
+rolls = []
 
-    	for i in range(6):
-        	for i in range(3):
-             	rolls.append(randrange(1,7))
-             	rolls.sort()
-        variables.append(str(sum(rolls[-3:])))
-    await client.say("Your ability scores are: " + ', '.join(variables))
+    for i in range(3):
+        rolls.append(randrange(1,7))
+        rolls.sort()
+        variabele1 = sum(rolls[-3:])
+    for i in range(3):
+        rolls.append(randrange(1,7))
+        rolls.sort()
+        variabele2 = sum(rolls[-3:])
+    for i in range(3):
+        rolls.append(randrange(1,7))
+        rolls.sort()
+        variabele3 = sum(rolls[-3:])
+    for i in range(3):
+        rolls.append(randrange(1,7))
+        rolls.sort()
+        variabele4 = sum(rolls[-3:])        
+    for i in range(3):
+        rolls.append(randrange(1,7))
+        rolls.sort()
+        variabele5 = sum(rolls[-3:])
+    for i in range(3):
+        rolls.append(randrange(1,7))
+        rolls.sort()
+        variabele6 = sum(rolls[-3:])    
+    await client.say("Your ability scores are: " + str(variabele1) + ", " + str(variabele2) + ", " + str(variabele3) + ", " + str(variabele4) + ", " + str(variabele5) + ", "  + str(variabele6))
+        
+                                 
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
+
+token = os.environ.get("DISCORD_BOT_SECRET")
+client.loop.create_task(list_servers())
+client.run(token)
+
         
                                  
 async def list_servers():
