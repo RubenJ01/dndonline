f04f2de25f3b54bc9d91d1b75856a0a6ac0146a7
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-11-27 10:45:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -250,7 +250,7 @@ async def stats():
         rolls.append(randrange(1,6))
         rolls.sort()
         variabel6 = sum(rolls[-3:])    
-    await client.say("Your ability scores are: " float(variabel1), float(variabel2), float(variabel3), float(variabel4), float(variabel5), float(variabel6))
+    await client.say("Your ability scores are: " + variabel1 + variabel2 + variabel3 + variabel4 + variabel5 + variabel6)
         
                                  
 async def list_servers():
