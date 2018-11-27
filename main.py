3c6d1267d8f3e13eb4db3e5d88f155dd4f525d62
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-11-27 13:58:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,23 +12,8 @@ from discord import Game
 from discord.ext.commands import Bot 
 from random import randrange
     
-
 BOT_PREFIX = (";", "/")
 client = Bot(command_prefix=BOT_PREFIX)
-chat_filter = ["PINEAPPLE", "APPLE", "CHROME"]
-bypass_list = []
-
-@client.event
-async def on_message(message):
-    contents = message.content.split(" ") #contents is a list type
-    for word in contents:
-        if word.upper() in chat_filter:
-            if not message.author.id in bypass_list:
-                try:
-                    await client.delete_message(message)
-                    await client.send_message(message.channel, "**Hey!** You're not allowed to use that word here!")
-                except discord.errors.NotFound:
-                    return
 
 def randadv(bottom, top):
     """
