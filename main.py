149d4fcf7cb557a7ea42b91f53b2886f72d28a97
DaanVanIJcken <32981209+DaanVanIJcken@users.noreply.github.com>
2018-11-29 11:01:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -95,6 +95,7 @@ create_roller_function(
     when given no parameters 1d20 is rolled
     parameters can be formatted like so "5d3 4d2 1d21" or "10" the latter only works with single dice
     when rolled every single roll gets an advantage and the total is returned"""
+)
 	
 # @client.command(
 #     name="advantage",
