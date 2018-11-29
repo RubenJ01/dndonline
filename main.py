796e8c05d3018a6b83dbf663413a759f0ad8ecf3
DaanVanIJcken <32981209+DaanVanIJcken@users.noreply.github.com>
2018-11-29 09:03:13 +0100

    Update main.py
    'nother test

--- main.py
+++ main.py
@@ -198,7 +198,9 @@ def call_to_dnd_beyond(link, **kwargs):
 		print(f"https://www.dndbeyond.com/{text}/{name}")
 	return _func
 
-spell = call_to_dnd_beyond("spells", name="spell" ,brief="Get a reference to any spell that is listed in D&D")
+call_to_dnd_beyond("spells", name="spell" ,brief="Get a reference to any spell that is listed in D&D")
+
+
 
 # @client.command(brief="Get a reference to any spell that is listed in D&D" )
 # async def spell(*name):
