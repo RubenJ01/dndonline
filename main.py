43b64fd25a89476aea7465da3b112522bcb2f961
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-12-08 22:01:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -310,6 +310,10 @@ async def character():
 	if classesroll == "Fighter":    
 		fightingstyle = ["Archery (swap light crossbow and 20 bolts for a longbow and 20 arrows; at your option, also swap chain mail for leather armor)", "Defense", "Dueling", "Great Weapon Fighting (swap longsword and shield for a greataxe", "Protection", "Two-Weapon Fighting (swap longsword for two short swords)"]
 		fightingstyleroll = random.choice(fightingstyle)
+		equipment1 = ["Mace", "Scale mail", "Light crossbow and 20 bolts", "Shield", "Holy symbol"]
+		equipment1roll = random.choice(equipment1)
+		equipment2 = ["Backpack, sack, lantern, 2 oil flasks, tinderbox, 12 pitons, hammer, waterskin, rations (4 days), 5 gp", "Backpack, 2 sacks, 6 torches, 3 oil flasks, tinderbox, 10-foot pole, 50 feet of rope, waterskin, rations (4 days), steel mirror", "Backpack, 4 sacks, holy symbol or thieves’ tools, 12 pitons, 50 feet of rope, waterskin, rations (4 days)"]
+		equipment2roll = random.choice(equipment2)
 		await client.say(
 		"Hit points: 10 + your Constitution modifier" +     
 		"\n" + "Alignment: " + alignmentroll + 
@@ -318,9 +322,14 @@ async def character():
 		"\n" + "Proficiency's: " + str(proficiencyroll) + 
 		"\n" + "Class: " + classesroll + 
 		"\n" + "Ability scores: " + str(variabeles) +
+		"\n" + "Equipment packs: " + equipment1roll + ", " + equipment2roll +	
 		"\n" + "Fighting style: " + fightingstyleroll
 		)
 	if classesroll == "Rogue":
+		equipment1 = ["Mace", "Scale mail", "Light crossbow and 20 bolts", "Shield", "Holy symbol"]
+		equipment1roll = random.choice(equipment1)
+		equipment2 = ["Backpack, sack, lantern, 2 oil flasks, tinderbox, 12 pitons, hammer, waterskin, rations (4 days), 5 gp", "Backpack, 2 sacks, 6 torches, 3 oil flasks, tinderbox, 10-foot pole, 50 feet of rope, waterskin, rations (4 days), steel mirror", "Backpack, 4 sacks, holy symbol or thieves’ tools, 12 pitons, 50 feet of rope, waterskin, rations (4 days)"]
+		equipment2roll = random.choice(equipment2)
 		await client.say(
 		"Hit points: 8 + your Constitution modifier" +     
 		"\n" + "Alignment: " + alignmentroll + 
@@ -329,8 +338,13 @@ async def character():
 		"\n" + "Proficiency's: " + str(proficiencyroll) + 
 		"\n" + "Class: " + classesroll + 
 		"\n" + "Ability scores: " + str(variabeles)
+		"\n" + "Equipment packs: " + equipment1roll + ", " + equipment2roll 	
 		)
 	if classesroll == "Wizard":
+		equipment1 = ["Mace", "Scale mail", "Light crossbow and 20 bolts", "Shield", "Holy symbol"]
+		equipment1roll = random.choice(equipment1)
+		equipment2 = ["Backpack, sack, lantern, 2 oil flasks, tinderbox, 12 pitons, hammer, waterskin, rations (4 days), 5 gp", "Backpack, 2 sacks, 6 torches, 3 oil flasks, tinderbox, 10-foot pole, 50 feet of rope, waterskin, rations (4 days), steel mirror", "Backpack, 4 sacks, holy symbol or thieves’ tools, 12 pitons, 50 feet of rope, waterskin, rations (4 days)"]
+		equipment2roll = random.choice(equipment2)
 		cantrips = ["Acid splash", "Dancing lights", "Fire bolt", "Light", "Mage hand", "Minor illusion", "Posion spray", "Prestidigation", "Ray of frost", "Shocking grasp"]
 		cantripsroll = random.sample(cantrips, 3)
 		spells = ["Burning hands", "Charm person", "Comprehend languages", "Detect magic", "Disguise self", "Identify", "Mage armor", "Magic missile", "Shield", "Silent image", "Sleep", "Thunderwave",]
