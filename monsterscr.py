d4b77e4a6398de9d1ddeb929a5240bb281b3c842
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-12-15 23:48:54 +0100

    Update and rename command to monsterscr.py

---
+++ monsterscr.py
@@ -0,0 +1,13 @@
+crhalf = ["Ape",
+          "Black Bear",
+          "Cockatrice",
+          "Crocodile",
+          "Darkmantle",
+          "Deep Gnome",
+          "Dust mephit",
+          "Gas spore",
+          "Giant goat",
+          "Giant sea horse",
+          "Giant wasp",
+          "Gnoll",
+          ]
