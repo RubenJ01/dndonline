1243c398dd7d9a16346a145a24044a233ba4e894
DaanVanIJcken <32981209+DaanVanIJcken@users.noreply.github.com>
2018-12-15 12:27:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -73,7 +73,7 @@ def create_roller_function(name, roller, good_roll_text="you managed to roll a f
 					sum_ += roll
 					rolls[-1].append(str(roll))
 				if s:
-					embed.add_field(name=f", you also rolled {die[0]}d{die[1]}", value="which became "+'+'.join(rolls[-1]))
+					embed.add_field(name=f"you also rolled {die[0]}d{die[1]}", value="which became "+'+'.join(rolls[-1]))
 				else:
 					embed.add_field(name=f"You rolled {dice[0][0]}d{dice[0][1]}", value="which became "+'+'.join(rolls[-1]))
 					s = 1
