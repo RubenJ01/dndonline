a784fa6d79add735dcf5a68106b5515e07157da5
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-12-17 14:51:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -566,7 +566,14 @@ async def encounter(level, size):
 	    if size == 6:
 		enc = random.choice(cr3)
 		print("You encounter: " + str(enc))
-	
+
+async def list_servers():
+    await client.wait_until_ready()
+    while not client.is_closed:
+        print("Current servers:")
+        for server in client.servers:
+            print(server.name)
+        await asyncio.sleep(600)
 			
 token = os.environ.get("DISCORD_BOT_SECRET")
 client.loop.create_task(list_servers())
