9693a28181ded041bb1b29e261cef45c5e729b18
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-12-22 00:59:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from random import sample
 from discord.ext.commands import Bot 
 from random import randrange
 from monsterscr import *
+from npcgen import *
 import random
 
 BOT_PREFIX = (";", "/")
@@ -137,42 +138,7 @@ create_roller_function(
 
 @client.command(name="npc", brief="create a quick npc" )
 async def npc():
-	background = ["Acolyte", "Criminal", "Folk hero", "Noble", "Sage", "Soldier"]
-	backgroundroll = random.choice(background)
-	traits = ["Nervous", "Excited", "Clumsy", "Distant", "Friendly", "Caring", "Anxious", "Laid-back", "Quiet", "Enthousiastic", "Mean", "Pridefull"]
-	traitsroll = random.choice(traits)
-	gender = ["Male", "Female"]
-	genderroll = random.choice(gender)
-	size = [3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7]
-	sizeroll = random.choice(size)
-	hair = ["Long", "Short"]
-	hairroll = random.choice(hair)
-	haircolour = ["blonde", "black", "white", "orange"]
-	haircolourroll = random.choice(haircolour)
-	race = ["Hill dwarf", "Mountain dwarf", "High elf", "Wood elf", "Lightfoot halfling", "Stout halfling"]
-	raceroll = random.choice(race)
-	age = random.randint(20, 100)
-	rolls = []
-	variabeles = []
-	for j in range(6):	
-		for i in range(4):
-			rolls.append(randint(1, 6))
-			rolls.sort() 	    
-		variabeles.append(sum(rolls)-min(rolls))
-		rolls =[]
-	embed = discord.Embed(
-		colour = discord.Colour.blue()
-	)
-	embed.set_author(name="Randomly generated NPC:")
-	embed.add_field(name="Race:", value=raceroll, inline=False)
-	embed.add_field(name="Gender:", value=genderroll, inline=False)
-	embed.add_field(name="Age:", value=age, inline=False)
-	embed.add_field(name="Traits:", value=traitsroll, inline=False)
-	embed.add_field(name="Background:", value=backgroundroll, inline=False)
-	embed.add_field(name="Hair:", value=hairroll + ", " + haircolourroll, inline=False)
-	embed.add_field(name="Size:", value=sizeroll, inline=False)
-	embed.add_field(name="Ability scores:", value=', '.join([str(v) for v in variabeles]), inline=False)
-	await client.say(embed=embed)
+	npcgen()
 
 				    
 # @client.command(brief="Generates a random encounter")
