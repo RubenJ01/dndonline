3678528a8a58a93919b6077630f93c8fa6d4c47a
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-12-21 15:52:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -491,10 +491,80 @@ async def help(ctx):
 	embed.add_field(name="Help", value="Sended you a private message which contains the information", inline=False)
 	await client.say(embed=embed)
 	
-@client.command(brief="A simple test")
-async def testme(level):
-	if level == "1":
-		await client.say("test succesfull!")
+@client.command(brief="An encounter generator for d&d 5e")
+async def encounter(level, size):
+	if level == "1": 	
+		if size == "2":
+			enc = random.sample(crkwart, 2)
+			await client.say("You encounter: " + str(enc))
+		if size == "3":
+			enc = random.sample(crkwart, 3)
+			await client.say("You encounter: " + str(enc))
+		if size == "4":
+			enc = random.sample(crkwart, 4)
+			await client.say("You encounter: " + str(enc))
+		if size == "5":
+			enc = random.sample(crkwart, 4)
+			enc2 = random.choice(cr1)
+			await client.say("You encounter: " + str(enc) + ", or you encounter: " + str(enc2))
+		if size == "6":
+			enc = random.sample(crkwart, 1)
+			enc2 = random.choice(cr1)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+		if size == "7":
+			enc = random.sample(crkwart, 2)
+			enc2 = random.choice(cr1)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+		if size == "8":
+			enc = random.sample(crkwart, 3)
+			enc2 = random.choice(cr1)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+	if level == "2":
+		if size == "2":
+			enc = random.sample(crhalf, 2)
+			await client.say("You encounter: " + str(enc))
+		if size == "3":
+			enc = random.sample(crhalf, 3)
+			enc2 = random.choice(cr1)
+			await client.say("You encounter: " + str(enc) + ", or you encounter: " + str(enc2))
+		if size == "4":
+			enc = random.sample(crhalf, 1)
+			enc2 = random.choice(cr1)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+		if size == "5":
+			enc = random.sample(crhalf, 2)
+			enc2 = random.choice(cr1)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+		if size == 6:
+			enc = random.sample(crhalf, 6)
+			enc2 = random.choice(cr2)
+			print("You encounter: " + str(enc) + ", or you encounter: " + str(enc2))
+		if size == "7":
+			enc = random.sample(crhalf, 1)
+			enc2 = random.choice(cr2)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+		if size == "8":
+			enc = random.sample(crhalf, 2)
+			enc2 = random.choice(cr2)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+	if level == "3":
+		if size == "2":
+			enc = random.sample(crhalf, 2)
+			await client.say("You encounter: " + str(enc))
+		if size == "3":
+			enc = random.sample(crhalf, 2)
+			enc2 = random.sample(crkwart, 1)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+		if size == "4":
+			enc = random.sample(cr2, 1)
+			await client.say("You encounter: " + str(enc))
+		if size == "5":
+			enc = random.sample(cr2, 1)
+			enc2 = random.sample(crhalf, 1)
+			await client.say("You encounter: " + str(enc) + ", " + str(enc2))
+		if size == "6":
+			enc = random.choice(cr3)
+			await client.say("You encounter: " + str(enc))
 		
 async def list_servers():
     await client.wait_until_ready()
