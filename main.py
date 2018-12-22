b8057fe5cab8628701c8178507dd055456145a04
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-12-22 01:13:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -138,7 +138,19 @@ create_roller_function(
 
 @client.command(name="npc", brief="create a quick npc" )
 async def npc():
-	npcgen()
+	embed = discord.Embed(
+		colour = discord.Colour.blue()
+	)
+	embed.set_author(name="Randomly generated NPC:")
+	embed.add_field(name="Race:", value=raceroll, inline=False)
+	embed.add_field(name="Gender:", value=genderroll, inline=False)
+	embed.add_field(name="Age:", value=age, inline=False)
+	embed.add_field(name="Traits:", value=traitsroll, inline=False)
+	embed.add_field(name="Background:", value=backgroundroll, inline=False)
+	embed.add_field(name="Hair:", value=hairroll + ", " + haircolourroll, inline=False)
+	embed.add_field(name="Size:", value=sizeroll, inline=False)
+	embed.add_field(name="Ability scores:", value=', '.join([str(v) for v in variabeles]), inline=False)
+
 	await client.say(embed=embed)
 
 				    
