f83a0a47983b67f5bf15cb6b0647a77f58e51b39
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2018-12-28 01:28:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -194,10 +194,10 @@ async def rngstat():
 	)
 	embed.set_author(name="Randomly generated ability scores")
 	for j in range(6):
-    		roll1 = int(random.randint(1,6))
-		roll2 = int(random.randint(1,6))
-		roll3 = int(random.randint(1,6))
-		roll4 = int(random.randint(1,6))
+    		roll1 = int(random.randint(1, 6))
+    		roll2 = int(random.randint(1, 6))
+   		roll3 = int(random.randint(1, 6))
+    		roll4 = int(random.randint(1, 6))
     		allroles = [roll1, roll2, roll3, roll4]
     		lowest = min(roll1, roll2, roll3, roll4)
     		ability = sum(allroles) - lowest
