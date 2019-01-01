27399a5a35bfbf4887bd9f4f1ea5f52fa2572e5d
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-01-01 13:46:55 +0100

    Updated ;help

--- main.py
+++ main.py
@@ -242,7 +242,7 @@ async def stop():
     await client.say(embed=embed)
 
 @client.command()
-async def restart():
+async def order():
     global initiative_roles
     embed = discord.Embed(
     	colour = discord.Colour.blue()
@@ -602,7 +602,7 @@ async def help(ctx):
 	embed.add_field(name=";rngstat3", value="Roll 1 ability score 3 times so players can use 1 for their definitive roll (best 3 rolls of a 4d6)", inline=False)
 	embed.add_field(name=";rngstat", value="Roll all 6 of your ability scores (best of 3 rolls of a 4d6 6 times)", inline=False)
 	embed.add_field(name=";currency", value="Calculate your total amount of pp gp sp cp respectively (format like ;currency 1gp 1pp 20sp etc)", inline=False)
-	embed.add_field(name=";initiative", value="Insert a name + modifier respectively and roll initiative multple names can be given", inline=False)
+	embed.add_field(name="Initiative tracker", value="The initiative tracker has 4 commands to help you keep track of initiative for your party:" + "\n" + ";initiative (charactername) (modifier) multiple names can be given, this starts the initiative tracker" + "\n" + ";next go to the next turn" + "\n" + ";back go to the previous turn" + "\n" + ";order see the order of rolled initaitives", inline=False)
 	embed.add_field(name=";character", value="Generate a quick level 1 character", inline=False)
 	embed.add_field(name=";npc", value="Generate a quick npc, including traits and appearence.", inline=False)
 	embed.add_field(name=";encounter", value="Make an encounter for your party (format like ;encounter party level party size so for example ;encounter 15 6 this will generate an encounter for a party of level 15 with 6 members, keep in mind that the minimum party size is 2 and the maximum is 8).", inline=False)
