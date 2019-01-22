7e9530f9a1c5fa2a659804727315f0d1580e2f55
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-01-22 20:45:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -402,55 +402,55 @@ async def rule(number):
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 0", value="No Malicious Behaviour" + "\n" + "Do not come in here with the intent to raid, brigade, or troll. Intentionally malicious users will be immediately and permanently banned. Come on, people, it’s common sense.", inline=False)
+		embed.add_field(name="Rule 0 - #welcome-rules", value="No Malicious Behaviour" + "\n" + "Do not come in here with the intent to raid, brigade, or troll. Intentionally malicious users will be immediately and permanently banned. Come on, people, it’s common sense.", inline=False)
 		await client.say(embed=embed)
 	if number == "1":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 1", value="No Obscene Content" + "\n" + "This is a SFW server. Any form of porn/hentai/etc, including links or pics, is forbidden. Erotic roleplay (ERP) is also strictly prohibited. If you must, take it to PMs and fade to black.", inline=False)
+		embed.add_field(name="Rule 1 - #welcome-rules", value="No Obscene Content" + "\n" + "This is a SFW server. Any form of porn/hentai/etc, including links or pics, is forbidden. Erotic roleplay (ERP) is also strictly prohibited. If you must, take it to PMs and fade to black.", inline=False)
 		await client.say(embed=embed)		
 	if number == "2":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 2", value="No Spam" + "\n" + " Posting of outside links has been disabled in most channels due to malicious user behaviour.  If you would like to post a link and cannot, ping (@) an online member of Staff and we will be happy to assist.", inline=False)
+		embed.add_field(name="Rule 2 - #welcome-rules", value="No Spam" + "\n" + " Posting of outside links has been disabled in most channels due to malicious user behaviour.  If you would like to post a link and cannot, ping (@) an online member of Staff and we will be happy to assist.", inline=False)
 		await client.say(embed=embed)
 	if number == "3":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 3", value="No Advertising" + "\n" + " Refrain from advertising your own content (YouTube, Twitch, Discord, Social Media, etc) in a public channel without written permission from the Staff. Exceptions may be made if it is specifically related to the channel and discussion you are in (e.g.: if you are an artist in #music-arts-crafts; answering a question in #player-help; if you have been approved for #streaming, etc). That said, PMing links to other users who have asked for them is permitted.", inline=False)
+		embed.add_field(name="Rule 3 - #welcome-rules", value="No Advertising" + "\n" + " Refrain from advertising your own content (YouTube, Twitch, Discord, Social Media, etc) in a public channel without written permission from the Staff. Exceptions may be made if it is specifically related to the channel and discussion you are in (e.g.: if you are an artist in #music-arts-crafts; answering a question in #player-help; if you have been approved for #streaming, etc). That said, PMing links to other users who have asked for them is permitted.", inline=False)
 		await client.say(embed=embed)
 	if number == "4":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 4", value="Be Civil" + "\n" + "You are free to engage in polite discussions and intellectual debates; in fact, we encourage it - passionate users are the best! However, avoid sliding into angry public arguments; those belong in your PMs.", inline=False)
+		embed.add_field(name="Rule 4 - #welcome-rules", value="Be Civil" + "\n" + "You are free to engage in polite discussions and intellectual debates; in fact, we encourage it - passionate users are the best! However, avoid sliding into angry public arguments; those belong in your PMs.", inline=False)
 		await client.say(embed=embed)
 	if number == "5":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 5", value="No Bullying" + "\n" + "Banter and teasing are fine, as long as it’s in good fun. However, discrimination or hate speech based on race, sex, gender, age, or sexuality is unacceptable. Racial slurs are specifically prohibited. If you are being bullied/harassed (even in PMs), feel free to report it to any member of Staff.", inline=False)
+		embed.add_field(name="Rule 5 - #welcome-rules", value="No Bullying" + "\n" + "Banter and teasing are fine, as long as it’s in good fun. However, discrimination or hate speech based on race, sex, gender, age, or sexuality is unacceptable. Racial slurs are specifically prohibited. If you are being bullied/harassed (even in PMs), feel free to report it to any member of Staff.", inline=False)
 		await client.say(embed=embed)
 	if number == "6":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 6", value="Complaints Are Welcome" + "\n" + "If you have a complaint about Staff or user behaviour, you are welcome to PM any online Staff at any level. If your complaint pertains to a member of Staff, take it one level higher to a Bartender, Innkeeper or Lord, as appropriate. If possible bring evidence of misconduct, such as a screenshot, since it will make our job significantly easier! If the evidence is edited/deleted, contact a Lord, who can check the Deleted Messages Archive.", inline=False)
+		embed.add_field(name="Rule 6 - #welcome-rules", value="Complaints Are Welcome" + "\n" + "If you have a complaint about Staff or user behaviour, you are welcome to PM any online Staff at any level. If your complaint pertains to a member of Staff, take it one level higher to a Bartender, Innkeeper or Lord, as appropriate. If possible bring evidence of misconduct, such as a screenshot, since it will make our job significantly easier! If the evidence is edited/deleted, contact a Lord, who can check the Deleted Messages Archive.", inline=False)
 		await client.say(embed=embed)
 	if number == "7":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 7", value="Respect Staff Decisions" + "\n" + "The Staff reserve the right to make decisions at their own discretion. That said, if you are being unfairly treated, please bring it to the attention of a higher Staff member, as per Rule 6. Not even Staff are above the law.", inline=False)
+		embed.add_field(name="Rule 7 - #welcome-rules", value="Respect Staff Decisions" + "\n" + "The Staff reserve the right to make decisions at their own discretion. That said, if you are being unfairly treated, please bring it to the attention of a higher Staff member, as per Rule 6. Not even Staff are above the law.", inline=False)
 		await client.say(embed=embed)
 	if number == "8":
 		embed = discord.Embed(
  		colour = discord.Colour.blue()
  		)
-		embed.add_field(name="Rule 8", value="No Impersonation" + "\n" + "Do not attempt to impersonate server Staff. The job is thankless and the Innkeeper pays us in Copper Pieces, if at all. Don’t make our lives harder.", inline=False)
+		embed.add_field(name="Rule 8 - #welcome-rules", value="No Impersonation" + "\n" + "Do not attempt to impersonate server Staff. The job is thankless and the Innkeeper pays us in Copper Pieces, if at all. Don’t make our lives harder.", inline=False)
 		await client.say(embed=embed)
 		
 		
