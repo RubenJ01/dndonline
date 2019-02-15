06111453465c7f407dddc142f7418d2ef0d3485d
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-15 10:34:38 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,5 +1,20 @@
-
+import random
+import os
+import asyncio
+import operator
+import random
+from random import randint
+from random import sample
+import json
+import discord
+from discord.ext.commands import Bot 
+from dumpfiles.monsterscr import *
+from dumpfiles.npcgen import *
 
 class RNG():
     def __init__(self, bot):
         self.bot = bot
+
+@commands.command()
+async def ping(self):
+    await client.say("pong")
