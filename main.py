9cf82f71109a3bb36f85f35602ab302eecf783ce
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-15 10:37:11 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,16 +16,6 @@ BOT_PREFIX = (";", "/t", "!t")
 client = Bot(command_prefix=BOT_PREFIX)
 client.remove_command("help")
 
-startup_extensions = ["rng"]
-
-if __name__ == "__main__":
-    for extension in startup_extensions:
-        try:
-            bot.load_extension(extension)
-        except Exception as e:
-            exc = '{}: {}'.format(type(e).__name__, e)
-            print('Failed to load extension {}\n{}'.format(extension, exc))
-
 @client.event
 async def on_ready():
     print("I'm in")
