527d923c32e6922563106d8c782817dd2bffb4d9
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-15 10:18:26 +0100

    Update test.py

--- corecommands/test.py
+++ corecommands/test.py
@@ -1,1 +1,21 @@
+import random
+import os
+import asyncio
+import operator
+import random
+from random import randint
+from random import sample
+import json
+import discord
+from discord.ext.commands import Bot 
+from dumpfiles.monsterscr import *
+from dumpfiles.npcgen import *
+
+class Fun:
+    def __init__(self, client):
+        self.client = client
+    
+    @commands.command()
+    async def ping(self):
+        await self.client.say('pong')
 
