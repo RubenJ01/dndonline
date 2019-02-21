d22b3b76541a5286f354eeb8c4a877c17939d1db
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-21 21:11:13 +0100

    Update command.py

--- command.py
+++ command.py
@@ -1,1 +1,14 @@
+import discord
+from discord.ext import commands
 
+class Command():
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def tester():
+        await self.bot.say('just a test')
+            
+              
+def setup(bot):
+    bot.add_cog(Members(bot))
