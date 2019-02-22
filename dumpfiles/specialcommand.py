35c2b58db9d3482b74aeba5111377a3cb1c37a63
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-22 12:10:00 +0100

    Update specialcommand.py

--- dumpfiles/specialcommand.py
+++ dumpfiles/specialcommand.py
@@ -1,1 +1,1 @@
-
+charge = "When you are specialised in a weapon with this property, you are able to, when moving at least 20 feet in a straight line towards a creature before hitting it with a weapon attack, double the damage modifier on the damage roll of this attack. The weapons with this property include: *Orc Double Axe, Spear, Halberd, Lance*"
