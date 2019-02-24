33023f24e7c763a2a6311ccee4e1bd8d8dce6654
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-24 13:07:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ startup_extensions = ['cogs.taverncogs.thelp',
 		      'cogs.taverncogs.faqcommand',
 		      'cogs.corecogs.normalhelp',
 		      'cogs.corecogs.encountergen',
-		      'cogs.taverncogs.rprule'
+		      'cogs.taverncogs.rprule',
 		      'cogs.corecogs.chargen']
 
 BOT_PREFIX = (";", "/t", "!t")
