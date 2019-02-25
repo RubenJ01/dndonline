0b37759486927ebe7c149658a4097b6865a0ed6c
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-25 13:09:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,40 +57,6 @@ async def on_member_join(member):
     content = "Welcome to The Tavern " + member.mention + ". " + stringspick
     channel = discord.utils.get(member.server.channels, name="general")
     await bot.send_message(channel, content)
-
-@bot.command(name='class', brief="get the details any 5e class")
-async def class_command(*argument):
-	classrequest = " ".join(argument)
-	classfinal = str.casefold(classrequest)
-	with open("databases/classes.json", "r") as classes_json:
-		data = json.load(classes_json, strict=False)
-	if classfinal in data:
-		class_data = data[classfinal]
-		hitdice = class_data['hit_dice']
-		hitpointslevel1 = class_data['hit_points_at_1st_level']
-		hitpointshigher = class_data['hit_points_at_higher_levels']
-		armor = class_data['armor']
-		weapons = class_data['weapons']
-		tools = class_data['tools']
-		savingthrows = class_data['saving_throws']
-		skills = class_data['skills']
-		equipment1 = class_data['equipment1']
-		equipment2 = class_data['equipment2']
-		equipment3 = class_data['equipment3']
-		quickbuild = class_data['quickbuild']
-		leveling = class_data['levelingtable']
-		embed = discord.Embed(
-			colour = discord.Colour.blue()
-		)
-		embed.set_author(name=f'The {classfinal} class')
-		embed.add_field(name="Hit points", value=f'Hit dice: {hitdice}' + "\n" + f'Hit points at 1st level: {hitpointslevel1}' + "\n" + f'Hit points at higher levels: {hitpointshigher}', inline=False)
-		embed.add_field(name="Proficiencies", value=f'Armor: {armor}' + "\n" + f'Weapons: {weapons}' + "\n" + f'Tools: {tools}' + "\n" + f'Saving throws: {savingthrows}' + "\n" + f'Skills: {skills}', inline=False)
-		embed.add_field(name="Equipment", value="You start with the following equipment, in addition to the equipment granted by your background:" + "\n" + str(equipment1) + "\n" + str(equipment2) + "\n" + str(equipment3), inline=False)
-		embed.add_field(name=f'The {classfinal}', value=leveling, inline=False) 
-		embed.add_field(name="Quick build", value=quickbuild, inline=False)
-		await bot.say(embed=embed)
-	else:
-		await bot.say("Class non-existent or missing")
 	
 def create_roller_function(name, roller, good_roll_text="a fabulous", **command_specifiers):
 	@bot.command(name=name, **command_specifiers)
