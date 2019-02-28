b708e51bd3d31689d94e8171cb5757bde8d8577c
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-28 19:48:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -143,35 +143,8 @@ create_roller_function(
 	lambda x, y: min(randint(x,y), randint(x,y), randint(x,y)), 
 	good_roll_text="despite your super-disadvantage you still managed to roll a(n)",
 	brief="Pick the worst of 3 rolls, same format as ;roll"
-)
-
-@bot.command(name="invite", brief="invite the bot to your discord server")
-async def invite():
-	embed = discord.Embed(
-		colour = discord.Colour.blue()
-	)
-	embed.add_field(name="Invite the bot to your server:", value="https://discordapp.com/oauth2/authorize?client_id=506541896630403080&scope=bot&permissions=0", inline=False)
-	await bot.say(embed=embed)
-
-	
-@bot.command(brief="information about the Tavern Bot")
-async def about():
-	embed = discord.Embed(
-		colour = discord.Colour.blue()
-	)
-	embed.set_author(name="About The Tavern Bot:")
-	embed.add_field(name="Date of creation:", value="18-11-2018", inline=False)
-	embed.add_field(name="Creators:", value="The Tavern Bot has been developed by: RubenJ01#0229 and Daan#2049")
-	embed.add_field(name="Contributors:", value="Thanks to: willdda117#2904 for contributing to The Tavern Bot")
-	embed.add_field(name="Source:", value="Since The Tavern Bot is open source you can check ouher repo: https://github.com/RubenJ01/dndonline", inline=False)
-	await bot.say(embed=embed)		     	
-			
-
-@bot.command(brief="link to the basic rules for d&d")
-async def basic():
-	await bot.say("http://media.wizards.com/2018/dnd/downloads/DnD_BasicRules_2018.pdf")					 
-					 
-	
+)	     							 
+					 	
 if __name__ == "__main__":
     for extension in startup_extensions:
         try:
