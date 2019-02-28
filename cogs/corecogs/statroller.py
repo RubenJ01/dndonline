48cfd38816cee01c545b9683fb9a65de053bcff1
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-28 19:36:11 +0100

    Create statroller.py

---
+++ cogs/corecogs/statroller.py
@@ -0,0 +1,63 @@
+import os
+import asyncio
+import operator
+import random
+from random import randint
+from random import sample
+import json
+import discord
+from discord.ext import commands
+
+class statroller():
+	def __init__(self, bot):
+        	self.bot = bot
+
+	@commands.command(brief="roll 6 random ability scores")
+	async def rngstat(self):
+		number = 0				 
+		embed = discord.Embed(
+			colour = discord.Colour.blue()
+		)
+		embed.set_author(name=" Randomly generated ability scores")
+		for j in range(6):
+			roll1 = int(random.randint(1, 6))
+			roll2 = int(random.randint(1, 6))
+			roll3 = int(random.randint(1, 6))
+			roll4 = int(random.randint(1, 6))
+			lowest = min(roll1, roll2, roll3, roll4)
+			allrolls = [roll1, roll2, roll3, roll4]
+			ability = sum(allrolls) - lowest
+			number = number + 1			 
+			embed.add_field(name="Roll " + str(number), value=str(roll1) + ", " + str(roll2) + ", " + str(roll3) + ", " + str(roll4) + " = " + str(ability), inline=False)
+		await self.bot.say(embed=embed)
+		
+	@commands.command(brief="roll 3 random ability scores")
+	async def rngstat3():
+		number = 0				 
+		embed = discord.Embed(
+			colour = discord.Colour.blue()
+		)
+		embed.set_author(name="3 randomly generated ability scores")
+		for j in range(3):
+			roll1 = int(random.randint(1, 6))
+			roll2 = int(random.randint(1, 6))
+			roll3 = int(random.randint(1, 6))
+			roll4 = int(random.randint(1, 6))
+			lowest = min(roll1, roll2, roll3, roll4)
+			allrolls = [roll1, roll2, roll3, roll4]
+			ability = sum(allrolls) - lowest
+			number = number + 1
+			embed.add_field(name="Roll " + str(number), value=str(roll1) + ", " + str(roll2) + ", " + str(roll3) + ", " + str(roll4) + " = " + str(ability), inline=False)
+		await self.bot.say(embed=embed)
+		
+	@commands.command(brief="roll a certain stat for example: dexterity")
+	async def stat(modifier=0):
+		embed = discord.Embed(
+			colour = discord.Colour.blue()
+		)
+		rolls = [randint(1,6) for _ in range(4)]
+		embed.add_field(name="Randomly rolled ability score", value=f"the total of the best 3 of your 4 rolls was {sum(rolls)-min(rolls)}", inline=False)
+		await self.bot.say(embed=embed)	
+
+def setup(bot):
+	bot.add_cog(status(bot))
