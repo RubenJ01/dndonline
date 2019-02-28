ca8d195b163d7f64e51ad52ed2ab1dab65aff546
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-28 13:59:42 +0100

    Update status.py

--- cogs/corecogs/status.py
+++ cogs/corecogs/status.py
@@ -15,10 +15,11 @@ class status():
 	@commands.command(brief="displays the amount of servers the bot is currently running in")
 	async def status(self):
 		members = len(list(bot.get_all_members()))
+		servers = len(bot.servers)
 		embed = discord.Embed(
 			colour = discord.Colour.blue()
 		)
-		embed.add_field(name="Bot status", value="Currently running in: " + str(members) + " members.", inline=False)
+		embed.add_field(name="Bot status", value="Currently running in: " + str(servers) + " servers with: " + str(members) + " members.", inline=False)
 		await self.bot.say(embed=embed)
 		
 def setup(bot):
