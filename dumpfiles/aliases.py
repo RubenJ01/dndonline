1ae5b423a7206666c2403ce5f762456670b2b9cf
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-02-28 19:45:02 +0100

    Create aliases.py

---
+++ dumpfiles/aliases.py
@@ -0,0 +1,2 @@
+"""This file sorts out all of the existing briefs, descriptions and aliases for current and future commands all in the proper order.
+Make sure you add new aliases, descriptions or briefs in the correct place when adding to this file."
