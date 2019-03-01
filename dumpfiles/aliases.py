02429ab2de8b40785fa19e2de05bf7b9b4b15539
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-01 17:00:13 +0100

    Update aliases.py

--- dumpfiles/aliases.py
+++ dumpfiles/aliases.py
@@ -2,37 +2,37 @@
 Make sure you add new aliases, descriptions or briefs in the correct place when adding to this file."""
 
 # about
-aboutbrief = ""
-aboutalias = []
-aboutdescription = ""
+aboutbrief = "Always been curious who made this bot? Find out by running this command."
+aboutalias = ['info']
+aboutdescription = "Gives an overview of who made the bot, who contributed it and when it got released. In this command current developers are shown as well as previous ones."
 # advantage
-advantagebrief = ""
-advantagealias = []
-advantagedescription = ""
+advantagebrief = "Rolls dices with advantage."
+advantagealias = ['adv']
+advantagedescription = "Rolls dices with advantage. Format like: ;advantage {dicetype}. Multiple dicetypes can be rolled."
 # basic
-basicbrief = ""
-basicealias = []
-basicdescription = ""
+basicbrief = "Quick reference to the 5e d&d rulebook."
+basicealias = ['basicrules', 'basicrule']
+basicdescription = "Quick reference to the 5e d&d rulebook."
 # character
-characterbrief = ""
-characteralias = []
-characterdescription = ""
+characterbrief = "Generate a level 1 character."
+characteralias = ['char', 'randchar']
+characterdescription = "Generates a level 1 character, with detailed stats, equipment, traits, appearance and much more."
 # class
-classbrief = ""
-classalias = []
-classdescription = ""
+classbrief = "Get the details of any 5e class."
+classalias = ['classes']
+classdescription = "Get the basic information of a 5e official class, homebrew not included. This does not get the information of feats, but only when you obtain them."
 # condition
-conditionbrief = ""
-conditionalias = []
-conditiondescription = ""
+conditionbrief = "Get the information about conditions in d&d 5e."
+conditionalias = ['con']
+conditiondescription = "Get the information about conditions in d&d 5e. The ones with tables have been changed as it can't be shown on discord."
 # currency
-currencybrief = ""
-currencyalias = []
-currencydescription = ""
+currencybrief = "Calculates your lower value currency, into the highest possible value."
+currencyalias = ['cur']
+currencydescription = "Calculates your lower value currency, into the highest possible value. Format like: ;currency {currency}, for example: ;currency 40sp 50gp."
 # disadvantage
-disadvantagebrief = ""
-disadvantagealias = []
-disadvantagdescription = ""
+disadvantagebrief = "Rolls dices with disadvantage"
+disadvantagealias = ['disadv']
+disadvantagdescription = "Rolls dices with disadvantage. Format like: ;disadvantage {dicetype}. Multiple dicetypes can be rolled."
 # encounter
 encounterbrief = ""
 encounteralias = []
