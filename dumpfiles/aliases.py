9ac6d63e8eca0ed89bccacb9b48c7dd1dd77b550
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-01 16:45:40 +0100

    Update aliases.py

--- dumpfiles/aliases.py
+++ dumpfiles/aliases.py
@@ -34,25 +34,50 @@ disadvantagebrief = ""
 disadvantagealias = []
 disadvantagdescription = ""
 # encounter
-
+encounterbrief = ""
+encounteralias = []
+encounterdescription = ""
 # invite
-
+invitebrief = ""
+invitealias = []
+invitedescription = ""
 # npc
-
+npcbrief = ""
+npcalias = []
+npcdescription = ""
 # rngstat
-
+rngstatbrief = ""
+rngstatalias = []
+rngstatdescription = ""
 # roll
-
+rngstatbrief = ""
+rngstatalias = []
+description = ""
 # rprule
-
+rprulebrief = ""
+rprulealias = []
+rpruledescription = ""
 # rule
-
+rulebrief = ""
+rulealias = []
+ruledescription = ""
 # spell
-
+spellbrief = ""
+spellalias = []
+spelldescription = ""
 # status
-
+statusbrief = ""
+statusalias = []
+statusdescription = ""
 # super-advantage
-
+superadvantagebrief = ""
+superadvantagealias = []
+superadvantagedescription = ""
 # super-disadvantage
-
+superdisadvantagebrief = ""
+superdisadvantagealias = []
+superdisadvantagedescription = ""
 # tavernhelp
+tavernhelpbrief = ""
+tavernhelpalias = []
+tavernhelpdescription = ""
