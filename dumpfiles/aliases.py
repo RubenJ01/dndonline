c4ff2ead0660a35402c73a2b1e857685987291b4
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-01 13:11:13 +0100

    Update aliases.py

--- dumpfiles/aliases.py
+++ dumpfiles/aliases.py
@@ -1,2 +1,42 @@
 """This file sorts out all of the existing briefs, descriptions and aliases for current and future commands all in the proper order.
 Make sure you add new aliases, descriptions or briefs in the correct place when adding to this file."
+
+# about
+
+# advantage
+
+# basic
+
+# character
+
+# class
+
+# condition
+
+# currency
+
+# disadvantage
+
+# encounter
+
+# invite
+
+# npc
+
+# rngstat
+
+# roll
+
+# rprule
+
+# rule
+
+# spell
+
+# status
+
+# super-advantage
+
+# super-disadvantage
+
+# tavernhelp
