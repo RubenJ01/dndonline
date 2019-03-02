74c631c69a21234061129a6c556e73ce1fbc3ae9
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-02 13:55:43 +0100

    Update npcgen.py

--- cogs/corecogs/npcgen.py
+++ cogs/corecogs/npcgen.py
@@ -1,12 +1,13 @@
 from dumpfiles.npcgen import *
 import discord
 from discord.ext import commands
+from dumpfiles.aliases import *
 
 class npcgen():
 	def __init__(self, bot):
         	self.bot = bot
 		
-	@commands.command(name="npc", brief="generate an npc that includes appearance, stats and traits" )
+	@commands.command(name="npc", brief=npcbrief, description=npcdescription)
 	async def npc(self):
 		embed = discord.Embed(
 			colour = discord.Colour.blue()
