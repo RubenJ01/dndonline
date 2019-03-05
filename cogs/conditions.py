c1f2900e119208a108a14c83dca5db9bb3b83008
RubenJ01 <45236226+RubenJ01@users.noreply.github.com>
2019-03-05 21:56:24 +0100

    moved faq to taverncogs

--- cogs/conditions.py
+++ cogs/conditions.py
@@ -3,24 +3,27 @@ from discord.ext import commands
 from dumpfiles.aliases import *
 import json
 
-class conditions():
-	def __init__(self, bot):
-        	self.bot = bot
 
-	@commands.command(brief=conditionbrief, description=conditiondescription)
-	async def condition(self, argument):
-		condition = str.casefold(argument)
-		with open("databases/conditions.json", "r") as conditions_json:
-			data = json.load(conditions_json)
-		if condition in data:
-			conditiondata = data[condition]
-			conditioncontent = conditiondata[content]
-			embed = discord.Embed(
-				colour = discord.Colour.blue()
-			)
-			embed.add_field(name=f'Condition {condition}', value=conditioncontent, inline=False)
-			await self.bot.say(embed=embed)
-		else:
-			await self.bot.say("Condition missing")
+class conditions():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(brief=conditionbrief, description=conditiondescription)
+    async def condition(self, argument):
+        condition = str.casefold(argument)
+        with open("databases/conditions.json", "r") as conditions_json:
+            data = json.load(conditions_json)
+        if condition in data:
+            conditiondata = data[condition]
+            conditioncontent = conditiondata[content]
+            embed = discord.Embed(
+                colour=discord.Colour.blue()
+            )
+            embed.add_field(name=f'Condition {condition}', value=conditioncontent, inline=False)
+            await self.bot.say(embed=embed)
+        else:
+            await self.bot.say("Condition missing")
+
+
 def setup(bot):
-	bot.add_cog(conditions(bot))
+    bot.add_cog(conditions(bot))
